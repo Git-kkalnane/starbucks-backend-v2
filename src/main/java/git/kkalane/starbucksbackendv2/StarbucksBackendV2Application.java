3306c55d0ed20adc4445789074adea66fba76377
Cori1304-Seong <andyjm7739@gmail.com>
2025-07-02 18:03:30 +0900

    first commit

---
+++ src/main/java/git/kkalane/starbucksbackendv2/StarbucksBackendV2Application.java
@@ -0,0 +1,13 @@
+package git.kkalane.starbucksbackendv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarbucksBackendV2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StarbucksBackendV2Application.class, args);
+  }
+
+}
---
+++ src/test/java/git/kkalane/starbucksbackendv2/StarbucksBackendV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package git.kkalane.starbucksbackendv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarbucksBackendV2ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
