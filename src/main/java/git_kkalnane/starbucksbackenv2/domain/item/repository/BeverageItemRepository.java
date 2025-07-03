67fa74e566fae115d14a8f85c9b13095ab552db6
Cori1304-Seong <andyjm7739@gmail.com>
2025-07-03 11:21:36 +0900

    feat: findAllWithSupportedSizes() 조회 기능 추가

--- src/main/java/git_kkalnane/starbucksbackenv2/domain/item/repository/BeverageItemRepository.java
+++ src/main/java/git_kkalnane/starbucksbackenv2/domain/item/repository/BeverageItemRepository.java
@@ -2,9 +2,14 @@ package git_kkalnane.starbucksbackenv2.domain.item.repository;
 
 import git_kkalnane.starbucksbackenv2.domain.item.domain.beverage.BeverageItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 
 @Repository
 public interface BeverageItemRepository extends JpaRepository<BeverageItem, Long> {
+    // id 오름차순 전체 목록 조회
+@Query("SELECT DISTINCT b FROM BeverageItem b LEFT JOIN FETCH b.supportedSizes")
+List<BeverageItem> findAllWithSupportedSizes();
 }
