3c64813b204f999a2bdf0ef5963abb618b2b2913
Cori1304-Seong <andyjm7739@gmail.com>
2025-07-03 13:46:57 +0900

    feat:  List<StoreSimpleDto> findAllSimpleStores() 추가

--- src/main/java/git_kkalnane/starbucksbackenv2/domain/store/repository/StoreRepository.java
+++ src/main/java/git_kkalnane/starbucksbackenv2/domain/store/repository/StoreRepository.java
@@ -1,9 +1,14 @@
 package git_kkalnane.starbucksbackenv2.domain.store.repository;
 
 import git_kkalnane.starbucksbackenv2.domain.store.domain.Store;
+import git_kkalnane.starbucksbackenv2.domain.store.dto.StoreSimpleDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface StoreRepository extends JpaRepository<Store, Long> {
+    @Query("SELECT new git_kkalnane.starbucksbackenv2.domain.store.dto.StoreSimpleDto(s.id, s.name, s.address, s.phone, s.openingHours, s.hasDriveThrough, s.seatingCapacity, s.latitude, s.longitude, s.imageUrl, s.crowdLevel) FROM Store s")
+    List<StoreSimpleDto> findAllSimpleStores();
 }
