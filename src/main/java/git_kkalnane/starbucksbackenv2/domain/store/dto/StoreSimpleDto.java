0fb3042c98625cb5168bbf7d26f169c368473701
Cori1304-Seong <andyjm7739@gmail.com>
2025-07-04 11:28:02 +0900

    feat: StoreSimpleDto 추가

---
+++ src/main/java/git_kkalnane/starbucksbackenv2/domain/store/dto/StoreSimpleDto.java
@@ -0,0 +1,21 @@
+package git_kkalnane.starbucksbackenv2.domain.store.dto;
+
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class StoreSimpleDto {
+    private Long id;
+    private String name;
+    private String address;
+    private String phone;
+    private String openingHours;
+    private Boolean hasDriveThrough;
+    private Integer seatingCapacity;
+    private BigDecimal latitude;
+    private BigDecimal longitude;
+    private String imageUrl;
+    private String crowdLevel;
+}
