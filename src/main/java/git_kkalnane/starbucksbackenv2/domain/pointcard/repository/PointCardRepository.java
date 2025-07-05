75f8df3b1742754ce67102b72bcc1100a4861da6
Cori1304-Seong <andyjm7739@gmail.com>
2025-07-05 16:33:25 +0900

    feat: Point 입급/출금시 DB에 존재하는지 확인하는 기능으로 변경

--- src/main/java/git_kkalnane/starbucksbackenv2/domain/pointcard/repository/PointCardRepository.java
+++ src/main/java/git_kkalnane/starbucksbackenv2/domain/pointcard/repository/PointCardRepository.java
@@ -1,6 +1,7 @@
 package git_kkalnane.starbucksbackenv2.domain.pointcard.repository;
 
 import git_kkalnane.starbucksbackenv2.domain.pointcard.domain.PointCard;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,7 @@ import org.springframework.stereotype.Repository;
 public interface PointCardRepository extends JpaRepository<PointCard, Long> {
 
     boolean existsByMemberId(Long id);
+
+    Optional<PointCard> findByMemberId(Long memberId);
 
 }
--- src/main/java/git_kkalnane/starbucksbackenv2/domain/pointcard/service/PointCardService.java
+++ src/main/java/git_kkalnane/starbucksbackenv2/domain/pointcard/service/PointCardService.java
@@ -1,13 +1,13 @@
 package git_kkalnane.starbucksbackenv2.domain.pointcard.service;
 
 import git_kkalnane.starbucksbackenv2.domain.member.domain.Member;
-import git_kkalnane.starbucksbackenv2.domain.paycard.common.exception.PayCardErrorCode;
-import git_kkalnane.starbucksbackenv2.domain.paycard.common.exception.PayCardException;
+import git_kkalnane.starbucksbackenv2.domain.pointcard.common.PointTransactionType;
+import git_kkalnane.starbucksbackenv2.domain.pointcard.common.exception.PointCardErrorCode;
+import git_kkalnane.starbucksbackenv2.domain.pointcard.common.exception.PointCardException;
 import git_kkalnane.starbucksbackenv2.domain.pointcard.domain.PointCard;
 import git_kkalnane.starbucksbackenv2.domain.pointcard.domain.PointTransaction;
 import git_kkalnane.starbucksbackenv2.domain.pointcard.repository.PointCardRepository;
 import git_kkalnane.starbucksbackenv2.domain.pointcard.repository.PointTransactionRepository;
-import git_kkalnane.starbucksbackenv2.domain.paycard.TransactionType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,50 +15,54 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class PointCardService {
+
     private final PointCardRepository pointCardRepository;
     private final PointTransactionRepository pointTransactionRepository;
 
-    private int initPointCardAmount=0;
+    private int initPointCardAmount = 0;
 
     /**
-     * 새로운 PayCard를 생성하고 초기 잔액을 설정합니다.
-     * 한 회원당 하나의 PayCard만 가질 수 있습니다.
+     * 새로운 PayCard를 생성하고 초기 잔액을 설정합니다. 한 회원당 하나의 PayCard만 가질 수 있습니다.
      *
      * @param member PayCard를 생성할 회원
      * @return 생성된 PayCard 객체
-     * @throws PayCardException 이미 PayCard가 존재하는 경우 발생
+     * @throws PointCardException 이미 PayCard가 존재하는 경우 발생
      */
     @Transactional
     public PointCard createPointCard(Member member) {
-        // 이미 PayCard가 있는지 확인
+        // 이미 PointCard가 있는지 확인
         if (pointCardRepository.existsByMemberId(member.getId())) {
-            throw new PayCardException(PayCardErrorCode.PAY_CARD_ALREADY_EXISTS);
+            throw new PointCardException(PointCardErrorCode.POINT_CARD_ALREADY_EXISTS);
         }
 
-        // 초기 잔액으로 PayCard 생성 및 저장
-       PointCard pointCard = PointCard.builder()
-           .cardNumber(member.getEmail()) // TODO card email 고민하기
-           .pointAmount(initPointCardAmount)
-           .member(member)
-        .build();
+        // 초기 잔액으로 PointCard 생성 및 저장
+        PointCard pointCard = PointCard.builder()
+            .cardNumber(member.getEmail()) // TODO card email 고민하기
+            .pointAmount(initPointCardAmount)
+            .member(member)
+            .build();
 
         return pointCardRepository.save(pointCard);
     }
-    
+
     /**
      * 포인트 입금 메서드
-     * @param pointCard 입금 대상 카드
-     * @param amount 입금할 포인트
+     *
+     * @param memberId 입금 대상 카드
+     * @param amount   입금할 포인트
      * @return 입금 후 PointCard
      */
     @Transactional
-    public PointCard addPoint(PointCard pointCard, int amount) {
+    public PointCard addPoint(Long memberId, int amount) {
+        PointCard pointCard = pointCardRepository.findByMemberId(memberId)
+            .orElseThrow(() -> new PointCardException(PointCardErrorCode.POINT_CARD_NOT_FOUND));
+
         pointCard.addPointAmount(amount);
         PointTransaction transaction = PointTransaction.builder()
             .pointCard(pointCard)
             .payAmount((long) amount)
-            .type(TransactionType.DEPOSIT)
-            .description("포인트 입금")
+            .type(PointTransactionType.DEPOSIT)
+            .description(getDescription(amount, PointTransactionType.DEPOSIT))
             .build();
         pointTransactionRepository.save(transaction);
         return pointCardRepository.save(pointCard);
@@ -66,21 +70,33 @@ public class PointCardService {
 
     /**
      * 포인트 출금 메서드
-     * @param pointCard 출금 대상 카드
-     * @param amount 출금할 포인트
+     *
+     * @param memberId 출금 대상 카드
+     * @param amount   출금할 포인트
      * @return 출금 후 PointCard
      */
     @Transactional
-    public PointCard subtractPoint(PointCard pointCard, int amount) {
+    public PointCard subtractPoint(Long memberId, int amount) {
+        PointCard pointCard = pointCardRepository.findByMemberId(memberId)
+            .orElseThrow(() -> new PointCardException(PointCardErrorCode.POINT_CARD_NOT_FOUND));
+        if (pointCard.getPointAmount() < amount) {
+            throw new PointCardException(PointCardErrorCode.NOT_ENOUGH_POINT_CARD_AMOUNT);
+        }
+        
         pointCard.subtractPointAmount(amount);
         PointTransaction transaction = PointTransaction.builder()
             .pointCard(pointCard)
             .payAmount((long) amount)
-            .type(TransactionType.WITHDRAWAL)
-            .description("포인트 출금")
+            .type(PointTransactionType.WITHDRAWAL)
+            .description(getDescription(amount, PointTransactionType.WITHDRAWAL))
             .build();
         pointTransactionRepository.save(transaction);
         return pointCardRepository.save(pointCard);
     }
+
+
+ private String getDescription(int amount, PointTransactionType type) {
+    return "" + amount + type.getKorean();
+ }   
 
 }
