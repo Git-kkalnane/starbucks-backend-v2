2093b6848949b9e85e8abfd123c5379299c290cb
Cori1304-Seong <andyjm7739@gmail.com>
2025-07-05 16:35:47 +0900

    refactor(PointCardErrorCode): String 값 변경

--- src/main/java/git_kkalnane/starbucksbackenv2/domain/pointcard/common/exception/PointCardErrorCode.java
+++ src/main/java/git_kkalnane/starbucksbackenv2/domain/pointcard/common/exception/PointCardErrorCode.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatus;
 public enum PointCardErrorCode implements ErrorCode {
     POINT_CARD_NOT_FOUND(HttpStatus.NOT_FOUND, "포인트 카드가 존재하지 않습니다."),
     POINT_CARD_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "이미 보유 중인 포인트 카드가 있습니다."),
-    NOT_ENOUGH_POINT_CARD_AMOUNT(HttpStatus.BAD_REQUEST, "결제 포인트의 잔액이 부족합니다."),
+    NOT_ENOUGH_POINT_CARD_AMOUNT(HttpStatus.BAD_REQUEST, "포인트 잔액이 부족합니다."),
     FAIL_POINT_TRANSACTION(HttpStatus.BAD_REQUEST, "포인트 사용 실패했습니다."),
     POINT_CARD_CREATION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "포인트 카드 생성에 실패했습니다. 다시 시도해주세요.");
 
