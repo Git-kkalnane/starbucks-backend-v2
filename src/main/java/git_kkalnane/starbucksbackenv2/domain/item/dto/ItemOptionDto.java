9761f54101c3ad5cb1df1aafb0400c52799a9ef8
Kim Yeong Ryeol <freekyr7529@gmail.com>
2025-07-08 21:28:15 +0900

    feat(ItemOptionDto.java): 아이템 옵션 DTO 구현

---
+++ src/main/java/git_kkalnane/starbucksbackenv2/domain/item/dto/ItemOptionDto.java
@@ -0,0 +1,14 @@
+package git_kkalnane.starbucksbackenv2.domain.item.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class ItemOptionDto {
+
+    private String name;
+    private Integer optionPrice;
+}
