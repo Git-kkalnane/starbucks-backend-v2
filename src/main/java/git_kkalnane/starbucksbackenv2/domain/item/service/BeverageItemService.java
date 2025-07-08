f9e32d6c289042cd8b03e00f4199cd3fe3d28baa
Kim Yeong Ryeol <freekyr7529@gmail.com>
2025-07-08 21:36:06 +0900

    fix(BeverageItemService.java): 단일 음료 조회 서비스 레이어 구현

--- src/main/java/git_kkalnane/starbucksbackenv2/domain/item/service/BeverageItemService.java
+++ src/main/java/git_kkalnane/starbucksbackenv2/domain/item/service/BeverageItemService.java
@@ -1,5 +1,7 @@
 package git_kkalnane.starbucksbackenv2.domain.item.service;
 
+import git_kkalnane.starbucksbackenv2.domain.item.common.exception.ItemErrorCode;
+import git_kkalnane.starbucksbackenv2.domain.item.common.exception.ItemException;
 import git_kkalnane.starbucksbackenv2.domain.item.domain.beverage.BeverageItem;
 import git_kkalnane.starbucksbackenv2.domain.item.dto.BeverageItemDto;
 import git_kkalnane.starbucksbackenv2.domain.item.dto.response.BeveragePaginationResponse;
@@ -16,8 +18,13 @@ public class BeverageItemService {
 
     private final BeverageItemRepository beverageItemRepository;
 
+    /**
+     * 모든 음료(커피 포함) 목록을 조회, 정렬, 페이징하여 반환합니다.
+     *
+     * @param pageable 페이징 및 정렬 정보
+     * @return {@link BeveragePaginationResponse}
+     */
     public BeveragePaginationResponse getAllBeverageItems(Pageable pageable) {
-
         Page<BeverageItem> paginationBeverages = beverageItemRepository.findAllWithSupportedSizes(pageable);
 
         List<BeverageItemDto> beverageItemDtos = paginationBeverages.map(BeverageItemDto::new).getContent();
@@ -29,6 +36,24 @@ public class BeverageItemService {
             .pageSize(paginationBeverages.getSize()).build();
     }
 
+    /**
+     * ID로 음료 상세 정보를 조회합니다.
+     *
+     * @param id 조회할 음료 ID
+     * @return {@link BeverageItem} 객체
+     * @throws ItemException 해당 ID를 가진 음료를 DB에서 찾을 수 없는 경우
+     */
+    public BeverageItem getSpecificBeverage(Long id) {
+        return beverageItemRepository.findByIdWithSupportedSizes(id)
+            .orElseThrow(() -> new ItemException(ItemErrorCode.BEVERAGE_NOT_FOUND));
+    }
+
+    /**
+     * 음료의 ID 값들을 가지고 있는 리스트를 인수로 받아 BeverageItem 객체 리스트를 반환하는 메서드 (OrderService에서 사용)
+     *
+     * @param ids 음료의 ID 값들을 가지고 있는 List
+     * @return {@link BeverageItem} 객체 리스트
+     */
     public List<BeverageItem> getBeverageByIds(List<Long> ids) {
         return beverageItemRepository.findAllById(ids);
     }
