6c4a14750cb1fa5c0ef9713a16dec63bd6e531e5
Kim Yeong Ryeol <freekyr7529@gmail.com>
2025-07-08 17:50:25 +0900

    feat(BeverageItemService.java): 음료 목록 조회 서비스 레이어 구현

--- src/main/java/git_kkalnane/starbucksbackenv2/domain/item/service/BeverageItemService.java
+++ src/main/java/git_kkalnane/starbucksbackenv2/domain/item/service/BeverageItemService.java
@@ -1,19 +1,32 @@
 package git_kkalnane.starbucksbackenv2.domain.item.service;
 
 import git_kkalnane.starbucksbackenv2.domain.item.domain.beverage.BeverageItem;
+import git_kkalnane.starbucksbackenv2.domain.item.dto.BeverageItemDto;
+import git_kkalnane.starbucksbackenv2.domain.item.dto.response.BeveragePaginationResponse;
 import git_kkalnane.starbucksbackenv2.domain.item.repository.BeverageItemRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class BeverageItemService {
+
     private final BeverageItemRepository beverageItemRepository;
 
-    public List<BeverageItem> getAllBeverageItems() {
-        return beverageItemRepository.findAllWithSupportedSizes();
+    public BeveragePaginationResponse getAllBeverageItems(Pageable pageable) {
+
+        Page<BeverageItem> paginationBeverages = beverageItemRepository.findAllWithSupportedSizes(pageable);
+
+        List<BeverageItemDto> beverageItemDtos = paginationBeverages.map(BeverageItemDto::new).getContent();
+
+        return BeveragePaginationResponse.builder().beverages(beverageItemDtos)
+            .totalCount(paginationBeverages.getTotalElements())
+            .currentPage(paginationBeverages.getNumber())
+            .totalPages(paginationBeverages.getTotalPages())
+            .pageSize(paginationBeverages.getSize()).build();
     }
 
     public List<BeverageItem> getBeverageByIds(List<Long> ids) {
