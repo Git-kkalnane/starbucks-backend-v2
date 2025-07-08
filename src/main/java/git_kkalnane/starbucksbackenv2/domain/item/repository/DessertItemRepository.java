5de72c0dd38462adb5e420c713e38f58c1a3db3a
Kim Yeong Ryeol <freekyr7529@gmail.com>
2025-07-08 23:43:58 +0900

    feat(DessertItemRepository.java): 단일 디저트 조회 리파지토리 레이어 구현

--- src/main/java/git_kkalnane/starbucksbackenv2/domain/item/repository/DessertItemRepository.java
+++ src/main/java/git_kkalnane/starbucksbackenv2/domain/item/repository/DessertItemRepository.java
@@ -1,10 +1,11 @@
 package git_kkalnane.starbucksbackenv2.domain.item.repository;
 
 import git_kkalnane.starbucksbackenv2.domain.item.domain.dessert.DessertItem;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DessertItemRepository extends JpaRepository<DessertItem, Long> {
-
+    Optional<DessertItem> findById(Long id);
 }
