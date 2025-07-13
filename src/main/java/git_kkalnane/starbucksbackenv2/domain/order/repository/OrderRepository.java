08b53089fceb52231ef375be1101f1d5a8bbb801
Cori1304-Seong <andyjm7739@gmail.com>
2025-07-14 04:34:20 +0900

    feat: member join 추가

--- src/main/java/git_kkalnane/starbucksbackenv2/domain/order/repository/OrderRepository.java
+++ src/main/java/git_kkalnane/starbucksbackenv2/domain/order/repository/OrderRepository.java
@@ -5,10 +5,13 @@ import git_kkalnane.starbucksbackenv2.domain.order.domain.OrderStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
@@ -32,4 +35,11 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
      * [매장용] 특정 매장의 과거 주문 내역(완료/취소)을 페이지네이션하여 조회합니다.
      */
     Page<Order> findByStoreIdAndStatusIn(Long storeId, Collection<OrderStatus> statuses, Pageable pageable);
+
+    @Query("SELECT o FROM Order o " +
+        "JOIN FETCH o.member " +
+        "JOIN FETCH o.orderItems oi " +
+        "WHERE o.id = :orderId")
+    Optional<Order> findOrderWithItemsById(@Param("orderId") Long orderId);
+
 }
