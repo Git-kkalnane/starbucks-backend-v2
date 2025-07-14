ff4cb0b888d51c1da03dc7538760858dd1abf4ea
0tae <choiyt3465@naver.com>
2025-07-14 11:16:47 +0900

    feat : EnableAsync 설정

--- src/main/java/git_kkalnane/starbucksbackenv2/StarbucksBackendV2Application.java
+++ src/main/java/git_kkalnane/starbucksbackenv2/StarbucksBackendV2Application.java
@@ -2,8 +2,10 @@ package git_kkalnane.starbucksbackenv2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class StarbucksBackendV2Application {
 
   public static void main(String[] args) {
