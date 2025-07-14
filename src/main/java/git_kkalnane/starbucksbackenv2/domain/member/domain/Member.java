0f809315563756e877c99e2031afc259c6f35de2
Kim Yeong Ryeol <freekyr7529@gmail.com>
2025-07-14 16:53:40 +0900

    feat: 멤버 로직 활성화

--- src/main/java/git_kkalnane/starbucksbackenv2/domain/member/domain/Member.java
+++ src/main/java/git_kkalnane/starbucksbackenv2/domain/member/domain/Member.java
@@ -1,30 +1,66 @@
 package git_kkalnane.starbucksbackenv2.domain.member.domain;
 
-import git_kkalnane.starbucksbackenv2.domain.inqurey.domain.Inquiry;
 import git_kkalnane.starbucksbackenv2.domain.cart.domain.Cart;
-import git_kkalnane.starbucksbackenv2.domain.notification.domain.Notification;
+import git_kkalnane.starbucksbackenv2.domain.inqurey.domain.Inquiry;
 import git_kkalnane.starbucksbackenv2.domain.order.domain.Order;
 import git_kkalnane.starbucksbackenv2.domain.paycard.domain.PayCard;
 import git_kkalnane.starbucksbackenv2.domain.pointcard.domain.PointCard;
-import jakarta.persistence.*;
+import git_kkalnane.starbucksbackenv2.global.entity.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Entity
+@Table(name = "members")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-@ToString
-@Table(name = "members")
-public class Member extends git_kkalnane.starbucksbackenv2.global.entity.BaseTimeEntity {
+public class Member extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", length = 50)
+    private String name;
+
+    @Column(length = 50)
+    private String nickname;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @OneToMany(mappedBy = "member")
+    private List<Inquiry> inquiries;
+
+    @OneToMany(mappedBy = "member")
+    private List<Cart> carts;
+
+    @OneToMany(mappedBy = "member")
+    private List<PayCard> payCards;
+
+    @OneToOne(mappedBy = "member")
+    private PointCard pointCard;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders;
 
     @Builder
     Member(Long id, String name, String nickname, String email, String password,
-        List<Inquiry> inquiries, List<Cart> carts, List<PayCard> payCards,
-        PointCard pointCards, List<Order> orders) {
+           List<Inquiry> inquiries, List<Cart> carts, List<PayCard> payCards,
+           PointCard pointCard, List<Order> orders) {
 
         this.id = id;
         this.name = name;
@@ -38,35 +74,12 @@ public class Member extends git_kkalnane.starbucksbackenv2.global.entity.BaseTim
         this.orders = orders;
     }
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    public void modifyNickname(String nickname) {
+        this.nickname = nickname;
+    }
 
-    @Column(name = "name", length = 50)
-    private String name;
-
-    @Column(length = 50)
-    private String nickname;
-
-    @Column(length = 255, unique = true)
-    private String email;
-
-    @Column(name = "password", length = 255)
-    private String password;
-
-    @OneToMany(mappedBy = "member")
-    private java.util.List<Inquiry> inquiries;
-
-    @OneToMany(mappedBy = "member")
-    private java.util.List<Cart> carts;
-
-    @OneToMany(mappedBy = "member")
-    private java.util.List<PayCard> payCards;
-
-    @OneToOne(mappedBy = "member")
-    private PointCard pointCard;
-
-    @OneToMany(mappedBy = "member")
-    private java.util.List<Order> orders;
+    public void modifyPassword(String password) {
+        this.password = password;
+    }
 
 }
